42e99049b387cd27c29d4989fe0e6b4f5a816ed4
AbdiAbader <abdiprocode@gmail.com>
2023-04-27 09:27:03 +0300

    update on notation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
-""" task 0 """
+""" task 0 - filter_datum
+filter_datum should return the log message obfuscated:
+Arguments:
+    fields: a list of strings representing all fields to obfuscate"""
 
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """ filter_datum """
     for field in fields:
-        message = re.sub(field + f".*?{separator}", field + "=" + redaction, message)
+        message = re.sub(field + f".*?{separator}",
+                         field + "=" + redaction, message)
     return message
