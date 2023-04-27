f4d73a59978fa02d4f320a730fc3cafd4bb84f06
AbdiAbader <abdiprocode@gmail.com>
2023-04-27 14:55:47 +0300

    encrypt

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,9 @@ def hash_password(password: str) -> bytes:
     and return a salted, hashed password, which is a byte string.
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """compares or validated if the hashed password is match the plain password
+    """
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
