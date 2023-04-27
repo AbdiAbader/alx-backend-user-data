1bc547c567f38e177b3aa0ae27440167da570a01
AbdiAbader <abdiprocode@gmail.com>
2023-04-27 10:46:58 +0300

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -30,6 +30,6 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """ filter_datum """
     for field in fields:
-        message = re.sub(field + f".*?{separator}",
+        message = re.sub(field + ".*?" + separator,
                          field + "=" + redaction, message)
     return message
