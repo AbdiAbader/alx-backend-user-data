f277278d9027e364ef5c82eaf4dfc1190258e5d6
AbdiAbader <abdiprocode@gmail.com>
2023-04-27 09:23:02 +0300

    update on notation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,9 +2,9 @@
 """ task 0 """
 
 import re
+from typing import List
 
-
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """ filter_datum """
     for field in fields:
         message = re.sub(field + f".*?{separator}", field + "=" + redaction, message)
