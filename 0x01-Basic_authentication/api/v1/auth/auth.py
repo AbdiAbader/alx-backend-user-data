a5a5d250bd400efd8f22ebec5680112cd34d12a1
AbdiAbader <abdiprocode@gmail.com>
2023-05-02 15:38:17 +0300

    modify require_auth method

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,7 +11,14 @@ class Auth():
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """require auth"""
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths:
+            return False
+        for path in excluded_paths:
+            if path.endswith('/') and path[:-1] == path:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """authorization head"""
