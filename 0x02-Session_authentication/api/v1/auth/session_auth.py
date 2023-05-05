50ce626f56c9d94d2b25b70d1b0946c7dd80d197
AbdiAbader <abdiprocode@gmail.com>
2023-05-05 22:36:25 +0300

    creat session in memory using uuid

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,3 +8,12 @@ import uuid
 
 class SessionAuth(Auth):
     """Session authentication class"""
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """creates a Session ID for a user_id"""
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
