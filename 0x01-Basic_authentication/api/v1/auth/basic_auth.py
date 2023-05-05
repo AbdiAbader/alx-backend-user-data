5dcda6bbf4c5e4b2defd4741a691a4dc6f784f69
AbdiAbader <abdiprocode@gmail.com>
2023-05-05 18:03:55 +0300

    update class basic_auth

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,7 +2,7 @@
 """BasicAuth class that inherits from Auth"""
 from api.v1.auth.auth import Auth
 import base64
-
+from typing import TypeVar, str
 
 class BasicAuth(Auth):
     """BasicAuth class that inherits
@@ -34,4 +34,19 @@ class BasicAuth(Auth):
         if ':' not in decoded_base64_authorization_header:
             return None, None
         return decoded_base64_authorization_header.split(':', 1)
+    
+    def user_object_from_credentials(self, user_email: str, user_pwd: str) -> TypeVar('User'):
+        """that returns the User instance based on his email and password."""
+        if user_email is None or type(user_email) is not str:
+            return None
+        if user_pwd is None or type(user_pwd) is not str:
+            return None
+        try:
+            users = User.search({'email': user_email})
+        except:
+            return None
+        for user in users:
+            if user.is_valid_password(user_pwd):
+                return user
+        return None
     
