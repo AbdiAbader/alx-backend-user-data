4e19ad4c82f7f1509a2301d8b135e0e626eb822d
AbdiAbader <abdiprocode@gmail.com>
2023-05-09 20:57:06 +0300

    update a user '

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -48,3 +48,13 @@ class DB:
         if user is None:
             raise NoResultFound
         return user
+    
+    def update_user(self, user_id: int, **args) -> None:
+        """update user """
+        user = self.find_user_by(id=user_id)
+        for key, value in args.items():
+            if key not in user.__dict__:
+                raise ValueError
+            setattr(user, key, value)
+        self._session.commit()
+
