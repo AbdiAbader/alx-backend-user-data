26e0ecbb0cf0633f8e807a86c2ce111e48857b96
AbdiAbader <abdiprocode@gmail.com>
2023-05-10 22:25:12 +0300

    update auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,6 +59,8 @@ class Auth:
     def get_user_from_session_id(self, session_id: str) -> str:
         """get Users by their session id"""
         try:
+            if session_id is None:
+                return None
             res = self._db.find_user_by(session_id=session_id)
             return res
         except NoResultFound:
