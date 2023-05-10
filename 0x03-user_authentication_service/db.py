c84083215baac803359ab418175428325da6bb2e
AbdiAbader <abdiprocode@gmail.com>
2023-05-10 10:21:13 +0300

    correct colunm type

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -35,9 +35,10 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """method that return a new user
         """
-        newUser = User(email=email, hashed_password=hashed_password)
-        if newUser:
-            self._session.add(newUser)
+        try:
+            new_user = User(email=email, hashed_password=hashed_password)
+            self._session.add(new_user)
             self._session.commit()
-            return newUser
-        return None
+            return new_user
+        except Exception:
+            return None
