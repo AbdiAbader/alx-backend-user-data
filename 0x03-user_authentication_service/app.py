a73d60b815521c824dac03dde6cdcf2a10c7e05f
AbdiAbader <abdiprocode@gmail.com>
2023-05-10 14:27:18 +0300

    add update password method in auth

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -89,7 +89,7 @@ def update_password() -> str:
         "new_password": request.form.get('new_password')
     }
     try:
-        res = AUTH._db.find_user_by(email=user_info['email'])
+        res = AUTH._db.find_user_by(email=user_info['reset_token'])
         AUTH.update_password(res.id, user_info['reset_token'],
                              user_info['new_password'])
         return jsonify({"email": res.email, "message": "Password updated"}), 200
