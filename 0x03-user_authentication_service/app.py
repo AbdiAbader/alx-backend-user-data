7bd0345915ce1916cfbef0058bcaa7d60ae55718
AbdiAbader <abdiprocode@gmail.com>
2023-05-10 12:21:08 +0300

    add adduser endpoint

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,13 +2,31 @@
 """ App module """
 
 from flask import Flask, jsonify, request, abort, redirect
+from auth import Auth
 
 
+AUTH = Auth()
 app = Flask(__name__)
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world() -> str:
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def addusers() -> str:
+    """register user"""
+
+    user_type = {
+        "email": request.form.get('email'),
+        "password": request.form.get('password')
+    }
+    res = Auth.register_user(AUTH, user_type['email'], user_type['password'])
+    try:
+        return jsonify({"email": res.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
