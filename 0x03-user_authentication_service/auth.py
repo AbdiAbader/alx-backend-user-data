b12e672b420d3e60633ef7d76d23dfeb83564bc5
AbdiAbader <abdiprocode@gmail.com>
2023-05-10 12:27:14 +0300

    add validate method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -30,3 +30,12 @@ class Auth:
             raise ValueError(f"User {email} already exists")
         except NoResultFound:
             return self._db.add_user(email, _hash_password(password))
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """validte user using checkpw(bcrypt)"""
+        try:
+            res = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  res.hashed_password)
+        except NoResultFound:
+            return False
