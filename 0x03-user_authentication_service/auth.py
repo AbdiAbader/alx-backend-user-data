c1eeceb4dd01ee0220f35513583bbca18859c423
AbdiAbader <abdiprocode@gmail.com>
2023-05-10 11:53:18 +0300

    add reg user method to auth class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,7 +2,8 @@
 """Auth Module for user service"""
 
 import bcrypt
-
+from db import DB
+from user import User
 
 def _hash_password(password: str) ->str:
     """return hashed password
@@ -10,3 +11,19 @@ def _hash_password(password: str) ->str:
     if password is None:
         return
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+    
+    def register_user(self, email: str, password: str) -> User:
+        """register user
+        """
+        if self._db.find_user_by(email=email):
+            raise ValueError(f"User {email} already exists")
+        return self._db.add_user(email, _hash_password(password))
+        
